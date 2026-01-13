4609fb00b576e8cfa3e19f45f789d5b2b01911ee
SergioMuros2 <murosverasergio@gmail.com>
2026-01-13 12:50:29 +0100

    feat:Changed file manifest.py

--- odoo/addons/smv_instituto/__manifest__.py
+++ odoo/addons/smv_instituto/__manifest__.py
@@ -1,34 +1,24 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "smv_instituto",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
-    'description': """
-Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+'name': "smv_instituto",
+'summary': "Gestión de un instituto",
+'description': "El módulo de Gestión de Instituto permite administrar de forma centralizada "
+"la información académica y administrativa. Facilita el registro y control de alumnos, "
+"docentes, cursos y materias",
+'author': "Sergio Muros Vera",
+'website': "https://www.educa2.madrid.org/web/iescanaveral",
+'category': 'Uncategorized',
+'version': '0.1',
+# any module necessary for this one to work correctly
+'depends': ['base'],
+# always loaded
+'data': [
+# 'security/ir.model.access.csv',
+'views/views.xml',
+'views/templates.xml',
+],
+# only loaded in demonstration mode
+'demo': [
+'demo/demo.xml',
+],
 }
-
